a1e96417b4225fe03b2f28c2eca6cc53b6ead38a
devdecade45 <146927748+devdecade45@users.noreply.github.com>
2023-10-04 13:47:09 +0530

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,197 @@
+<html>
+<head>
+    <title>BioData</title>
+    <style>
+        h1{
+            text-align: center;
+            font-family:inherit;
+    }
+        h4{
+            float: right;
+            margin-right: 30px;           
+        }
+        #div1{
+            width: 800px;
+            height: 1125px;
+            border: solid black;
+            background-color:floralwhite;
+        }
+        #table1{
+            margin-left:30px;
+            float: left;
+            border: solid black;
+            width: 100px;
+            height: 140px;
+            background-image: url("passport.jpg");        }
+        #div2{
+          width: 700px;
+            height: 150px;
+            margin-left: 50px;g
+        }
+        #div3{
+            background-color: gray;
+            margin: 5px;
+            width: 780px;
+            height: 25px;
+        }
+        h3{
+            margin: 20px;
+            color: white;
+        }
+        #div4{
+            width: 780px;
+            height: 120px;
+            margin-left: 45px;
+        }
+        #table2{
+            margin-left: 80px;
+            margin-bottom: 30px;
+        }
+        #b1{
+            color:gray;
+        }
+        hr{
+           border: solid 1px black;
+            width: 780px;
+        }
+        #div5{
+            width: 780px;
+            height: 120px;
+        }
+        #table3{
+            margin-left: 29px;
+        }
+        #div6{
+            height: 30px;
+            }
+        #table9{
+            margin-top: 200px;
+        }
+        #div7{
+            margin-left: 17px;
+        }
+        img{
+          width=100px;
+          height=140px;  
+        }
+    </style>
+    </head>
+    <body bgcolor="yellow">
+    
+    </body>
+</html>
+<?php
+  echo"<div id='div1'><h1><center><u>Resume</u></center></h1>
+  <div id='div2'>
+  <table id='table1' width='100px' height='140px'>
+  <tr>
+  <td> </td>
+  </tr>
+  </table>
+  <h4>Plot No.023,Flat No.11<br>Murlidhar Nagar,<br>Pathardi,Nasik.<br>7028177879<br>devanshkhairnar03@gmail.com</h4></div>";
+    echo"<div id='div3'> 
+    <b><h3>GITESH KISHOR KHAIRNAR</h3></b>";
+    echo"<div id='div4'>
+    <table width='800px' height='100px'>
+    <tr>
+    <th width='150px'>
+       <b id='b1'>OBJECTIVE</b>
+    </th>
+     <br>
+    <td width='650px'>
+    To Work With Good Organization & Use My Skills In Increasing The <br> Performance Of The Organization And Gain New Experience
+    </td></tr></table>
+    </div><hr>";
+    echo"<div id='div5'>
+    <table width='800px' height='100px'>
+    <tr>
+    <th width='200px'>
+       <b id='b1'>SKILLS & ABILITIES </b>
+    </th>
+     <br>
+    <td width='600px'>
+     Strong ability to master a situation quickly, knowledgeble in a wide <br> 
+    variety of professional disciplines and an expert at organizing & directing <br>
+    turnaround situation <br>
+    Team Performance with strong communication and relationship <br>
+    management skills. <br>
+    Managing day-to-day activities in co-ordination with internal / <br>
+    external
+    </td></tr></table>
+    </div>";
+    echo"<div id='div6'></div>
+    <hr>
+    <div id='div6'>
+    <table width='800px' height='100px'>
+    <tr>
+    <th width='200px'>
+       <b id='b1'> EXPIRIENCE </b>
+    </th>
+     <br>
+    <td width='600px'>
+    <b> 
+    6+ YEARS' EXPIRIENCE IN COMPUTER HARDWARE AND COMPUTER SOFTWARE <br>
+    </b>
+    Administrator-Accuracy Computers At My Own Shop.
+    <table id='table5' width='500px'><tr><td></td><td></td></tr>
+    <tr><td></td><td></td> </tr>
+    </table>
+    <b> EXPIRIENCE AS SALES EXECUTIVE IN UNIQUE AUTOMOBILES AT DWARAKA </b> <br> All Office Related Documentation & Field Works.
+    <table id='table6' width='500px'><tr><td></td><td></td></tr>
+    <tr><td></td><td></td> </tr>
+    </table>
+    <b>
+    HARDWARE & NETWORK ENGINEER AT GROUND COMPUTER INSTITUTE
+    </b> <br>
+    All Computer Related Works,Installtion,Troubleshooting & Hardware Diagnostic
+    <table id='table7' width='500px'><tr><td></td><td></td></tr>
+    <tr><td></td><td></td> </tr>
+    </table>
+    <b>
+    Specialist In Gaming Hardware & Expirience In Call Center
+    </b>
+    </td></tr></table>
+    <hr></div>";
+        echo"<div id='div7'>
+        <table id='table9' width='800px' height='100px'>
+    <tr>
+    <th width='150px'>
+       <b id='b1'>TECHNICAL SKILLS</b>
+    </th>
+     <br>
+    <td width='650px'>
+   <ul> 
+   <li> Extensive Expirience With Microsoft Products And Operating <br>
+   Systems Such As Windows XP, Windows vista, Windows 7. 
+   <br>
+   <br>
+   </li>
+   <li> 
+   Knowledge Of Designing, Installing, And Configuration Local Area <br>
+   Networks, Designing Plus Simulating Wide Area Networks In A <br> 
+   Lab Environment
+   <br>
+   </li>
+   <br>
+   <li>
+   Deep Knowledge Of High End Computer Hardware And ASoftware
+   <br>
+   </li>
+   <br>
+   <li>
+   Expirience With Multi Monitor Setups With Multi GPU's and <br> CPU's
+   <br>
+   </li>
+   <br>
+   <li> 
+   Expirience In Computer Operating, Creating & Designing <br>
+   Business Cards, Video & Editing And Projects
+   </li>
+   </ul> 
+</td>
+</tr>
+</table>
+        </div>
+        </div>";
+        ?>
+</html> 
